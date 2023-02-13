c67704359f6a44ae6634bf933b0cdd9075fc8575
Nuno Gil Mesquita <nunomesquita2@gmail.com>
2023-02-13 15:48:18 +0000

    Multiple clients can send messages; need to ignore answer from server

--- src/Server.java
+++ src/Server.java
@@ -1,43 +1,52 @@
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Server {
-    public static void main(String[] args) {
-        ServerSocket serverSocket = null;
-        BufferedReader inputReader;
-        BufferedWriter outPut;
-        BufferedReader consoleInput;
 
-        Socket clientSocket;
+    public static void main(String[] args) {
+        ServerSocket serverSocket;
+        int id = 0;
         try {
             serverSocket = new ServerSocket(8080);
-
             while (true) {
-                clientSocket = serverSocket.accept();
+                ExecutorService chat = Executors.newCachedThreadPool();
+                final Socket clientSocket = serverSocket.accept();
 
-                System.out.println("new client");
+                id++;
 
-                inputReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                outPut = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
-                consoleInput = new BufferedReader(new InputStreamReader(System.in));
-                String msgReceived;
-                String msgToSend;
-                while ((msgReceived = inputReader.readLine()) != null) {
-
-                    System.out.println("Message received: ".concat(msgReceived));
-
-                    msgToSend = consoleInput.readLine();
-
-                    outPut.write(msgToSend);
-                    outPut.newLine();
-                    outPut.flush();
-
+                for (int i = 0; i < id; i++) {
+                    chat.submit(new Thread(() -> {
+                        try {
+                            newClient(clientSocket);
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }));
                 }
-
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    private static void newClient(Socket socket) throws IOException {
+        BufferedReader inputReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        BufferedWriter outPut = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+        BufferedReader consoleInput= new BufferedReader(new InputStreamReader(System.in));
+        String msgReceived;
+        String msgToSend;
+        while ((msgReceived = inputReader.readLine()) != null) {
+
+            System.out.println("Message received: ".concat(msgReceived));
+
+            msgToSend = consoleInput.readLine();
+
+            outPut.write(msgToSend);
+            outPut.newLine();
+            outPut.flush();
         }
     }
 }
