974c07d51c30119732a39f59279f2e722d801b77
GutoFaria365 <122914909+GutoFaria365@users.noreply.github.com>
2023-02-14 00:57:50 +0000

    FUlly working com EXIT e Historico

--- src/ClientExperimental.java
+++ src/ClientExperimental.java
@@ -4,12 +4,14 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class ClientExperimental {
+    public static final String RESET = "\u001B[0m";
+    public static final String BLUE = "\033[34m";
+
     public static void main(String[] args) {
         Socket socket = null;
         BufferedReader inputReader;
         BufferedWriter output;
         BufferedReader consoleInput;
-        boolean firstTime;
         try {
             socket = new Socket("localhost", 8080);
             consoleInput = new BufferedReader(new InputStreamReader(socket.getInputStream()));
@@ -23,7 +25,6 @@ public class ClientExperimental {
                 String msgToSend = inputReader.readLine();
 
                 if (msgToSend.compareTo("EXIT") == 0) {
-                    socket.close();
                     break;
                 }
 
@@ -31,25 +32,33 @@ public class ClientExperimental {
                 output.newLine();
                 output.flush();
 
+                // Thread creation to always receive messages sent
                 ExecutorService viewMessage = Executors.newCachedThreadPool();
-
                 Socket finalSocket = socket;
+
                 viewMessage.submit(new Thread(() -> {
                     String msgReceived = null;
                     try {
                         BufferedReader consoleRead = new BufferedReader(new InputStreamReader(finalSocket.getInputStream()));
-                        while ((msgReceived = consoleRead.readLine()) != null) {
+                        while (!viewMessage.isShutdown() && (msgReceived = consoleRead.readLine()) != null) {
                             System.out.println(msgReceived);
                         }
+                    } catch (EOFException e) {
+                        viewMessage.shutdownNow();
+                        throw new RuntimeException(e);
                     } catch (IOException e) {
                         throw new RuntimeException(e);
+                    } finally {
+                        viewMessage.shutdownNow();
                     }
                 }));
+
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
         } finally {
             try {
+                System.out.println(BLUE + "You have been disconnected from the server" + RESET);
                 socket.close();
             } catch (IOException e) {
                 throw new RuntimeException(e);
--- src/ServerExperimental.java
+++ src/ServerExperimental.java
@@ -7,25 +7,33 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class ServerExperimental {
+    public static final String RESET = "\u001B[0m";
+    public static final String RED = "\u001B[31m";
+    public static final String BLUE = "\033[34m";
+    public static final String YELLOW = "\033[33m";
     private static List<Socket> clientConnections = new ArrayList<>();
 
     public static void main(String[] args) {
         ServerSocket serverSocket;
         try {
             serverSocket = new ServerSocket(8080);
+            writeHistory("\nNEW SESSION");
+
             while (true) {
                 ExecutorService chat = Executors.newCachedThreadPool();
                 final Socket clientSocket = serverSocket.accept();
 
-
                 BufferedReader consoleInput = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
                 BufferedWriter outputName = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
-                outputName.write("Please insert your username");
+                outputName.write(YELLOW + "Please insert your username" + RESET);
                 outputName.newLine();
                 outputName.flush();
 
                 String userName = consoleInput.readLine();
-                System.out.println(userName.concat(" has joined the server"));
+                broadcastMessage(BLUE + userName.concat(" has joined the server") + RESET);
+                System.out.println(BLUE + userName.concat(" has joined the server" + RESET).toUpperCase());
+                writeHistory(userName.concat(" has joined the server").toUpperCase());
+
 
                 chat.submit(new Thread(() -> {
                     try {
@@ -41,17 +49,23 @@ public class ServerExperimental {
     }
 
     private static void newClient(String user, Socket socket, List<Socket> socketList) throws IOException {
+
         socketList.add(socket);
+        System.out.println(RED + socketList + RESET);
         BufferedReader inputReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         String msgReceived;
 
         while ((msgReceived = inputReader.readLine()) != null) {
-            System.out.println("Message received: ".concat(msgReceived));
+            System.out.println("Message received from " + user.concat(": " + msgReceived));
 
-            // Broadcast the received message to all clients
             broadcastMessage(user + ": " + msgReceived);
+            writeHistory(user.concat(": " + msgReceived));
         }
         socketList.remove(socket);
+        broadcastMessage(YELLOW + user.concat(" has left the server").toUpperCase() + RESET);
+        writeHistory(user.concat(" has left the server").toUpperCase());
+        System.out.println(YELLOW + user.concat(" has left the server").toUpperCase() + RESET);
+        System.out.println(RED + socketList + RESET);
     }
 
     private static void broadcastMessage(String message) throws IOException {
@@ -61,5 +75,18 @@ public class ServerExperimental {
             outPut.newLine();
             outPut.flush();
         }
+    }
+
+    private static void writeHistory(String writeThis) {
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter("serverHistory.txt", true));
+
+            writer.write(writeThis);
+            writer.newLine();
+            writer.close();
+        } catch (IOException e) {
+            System.out.println("Error writing to file: " + e.getMessage());
+        }
+
     }
 }
