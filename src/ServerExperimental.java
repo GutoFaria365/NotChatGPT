2f0bb83e17e2386a2384d328fb08f0b0bb9d655d
GutoFaria365 <122914909+GutoFaria365@users.noreply.github.com>
2023-02-14 10:43:28 +0000

    FULLY FUCKING WORKING

--- src/ServerExperimental.java
+++ src/ServerExperimental.java
@@ -2,7 +2,9 @@ import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -11,7 +13,9 @@ public class ServerExperimental {
     public static final String RED = "\u001B[31m";
     public static final String BLUE = "\033[34m";
     public static final String YELLOW = "\033[33m";
+    public static final String PINK = "\u001B[35m";
     private static List<Socket> clientConnections = new ArrayList<>();
+    private static HashMap<String, Socket> clientMap = new HashMap<>();
 
     public static void main(String[] args) {
         ServerSocket serverSocket;
@@ -35,37 +39,63 @@ public class ServerExperimental {
                 writeHistory(userName.concat(" has joined the server").toUpperCase());
 
 
-                chat.submit(new Thread(() -> {
+                Thread t = new Thread(() -> {
                     try {
-                        newClient(userName, clientSocket, clientConnections);
+                        newClient(userName, clientSocket, clientConnections, clientMap);
                     } catch (IOException e) {
                         throw new RuntimeException(e);
                     }
-                }));
+                });
+                chat.submit(t);
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
-    private static void newClient(String user, Socket socket, List<Socket> socketList) throws IOException {
-
+    private static void newClient(String user, Socket socket, List<Socket> socketList, HashMap<String, Socket> clientMap) throws IOException {
         socketList.add(socket);
+        clientMap.put(user, socket);
         System.out.println(RED + socketList + RESET);
         BufferedReader inputReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         String msgReceived;
 
         while ((msgReceived = inputReader.readLine()) != null) {
-            System.out.println("Message received from " + user.concat(": " + msgReceived));
+            if (msgReceived.startsWith("@")) {
+                msgReceived = msgReceived.substring(1);
+                privateMessage(socket, clientMap, msgReceived, user);
 
-            broadcastMessage(user + ": " + msgReceived);
-            writeHistory(user.concat(": " + msgReceived));
+            } else {
+                System.out.println("Message received from " + user.concat(": " + msgReceived));
+                broadcastMessage(user + ": " + msgReceived);
+                writeHistory(user.concat(": " + msgReceived));
+            }
         }
         socketList.remove(socket);
         broadcastMessage(YELLOW + user.concat(" has left the server").toUpperCase() + RESET);
         writeHistory(user.concat(" has left the server").toUpperCase());
         System.out.println(YELLOW + user.concat(" has left the server").toUpperCase() + RESET);
         System.out.println(RED + socketList + RESET);
+    }
+
+    private static void privateMessage(Socket socket, HashMap<String, Socket> clientMap, String pmName, String user) throws IOException {
+        BufferedReader inputPM = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        String pmReceived = inputPM.readLine();
+
+        for (Map.Entry<String, Socket> set : clientMap.entrySet()) {
+            if (set.getKey().compareTo(pmName) == 0) {
+                BufferedWriter outPut = new BufferedWriter(new OutputStreamWriter(set.getValue().getOutputStream()));
+                outPut.write(PINK + user.concat(" PM: ").concat(pmReceived) + RESET);
+                outPut.newLine();
+                outPut.flush();
+                return;
+            }
+        }
+        BufferedWriter outPut = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+        outPut.write(RED + "User doesn't exist" + RESET);
+        outPut.newLine();
+        outPut.flush();
+
     }
 
     private static void broadcastMessage(String message) throws IOException {
